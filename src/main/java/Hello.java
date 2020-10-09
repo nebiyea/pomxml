7ef99aef705cd85effc506b4704b1b148bae0774
Nebiye <nebiyearslan1@gmail.com>
2020-10-09 21:37:54 +0100

    ititial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,23 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Nebiye");
+        //*Create brandnew project on IntelliJ
+       // Enable version controignore the files you need to ignore
+       // make initial commit and push to Github
+        //Add new branch locally called feature1
+       // add more changes commit and push to Github
+        //Compare your origin/master to origin/feature1
+       // Create a pull request
+        //now you have option to merge your origin/feature1
+        //into the master
+        //now pull the changes into your local master branch
+                //
+    }
+
+
+}
+
+
