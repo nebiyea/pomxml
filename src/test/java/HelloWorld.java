3a5adfbc8149d7495a1bcf91a35f6576f015763f
Nebiye <nebiyearslan1@gmail.com>
2020-10-13 18:41:30 +0100

    update the file

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("nebiye is coming like perfect");
+        System.out.println("this my own hello");// this my hello, and c
 
     }
 }
